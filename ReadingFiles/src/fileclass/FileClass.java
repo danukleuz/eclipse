16ef802bc171c746d805891e7d850e30248acf91
Da Nuke <steverocklow@gmail.com>
2019-09-10 02:49:49 -0400

    Initial commit

---
+++ ReadingFiles/src/fileclass/FileClass.java
@@ -0,0 +1,42 @@
+package fileclass;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class FileClass {
+
+	public static void main(String[] args)  {
+
+
+		File f = new File("C:\\Users\\Name\\Desktop\\Files\\file.rtf");
+		
+		try(PrintWriter writer = new PrintWriter(f)){ //try with resources auto closes the resource
+			writer.println("Yo man");
+			writer.println("Wassup");
+			writer.println("You chillin?");
+			writer.println("Nah nah nigga");
+			
+			
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found");
+		}
+		
+		try(Scanner sc = new Scanner(f)){
+			System.out.println(sc.next());
+			System.out.println(sc.next());
+			System.out.println(sc.next());
+			System.out.println(sc.nextLine());
+			System.out.println(sc.nextLine());
+			System.out.println(sc.nextLine());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("File not found");
+		}
+		
+
+	}
+
+}
